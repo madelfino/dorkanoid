e9df1fa0c9039d4fc378ac49b44cda5bae9c84aa
Michael Delfino <madelfino@madelfino-VirtualBox.(none)>
2013-01-23 23:17:03 -0500

    Draw the things

--- src/Dorkanoid.cpp
+++ src/Dorkanoid.cpp
@@ -1,3 +1,5 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
 #include <iostream>
 
 void init()
@@ -10,9 +12,12 @@ bool update()
     return false;
 }
 
-void draw()
+void draw(sf::RenderWindow *window, std::vector<sf::Sprite> sprites)
 {
-    std::cout << "Hello World!" << std::endl;
+    window->Clear();
+    for (unsigned int i=0; i<sprites.size(); ++i)
+        window->Draw(sprites[i]);
+    window->Display();
 }
 
 void cleanup()
@@ -22,13 +27,58 @@ void cleanup()
 
 int main()
 {
-    init();
-    bool game = true;
-    while (game)
+    //init();
+
+    //create the game window
+    sf::RenderWindow canvas(sf::VideoMode(800, 600), "Dorkanoid");
+
+    sf::Image BackgroundImage, PaddleImage, BallImage, BricksImage;
+    if (!BackgroundImage.LoadFromFile("assets/images/background.png") ||
+        !PaddleImage.LoadFromFile("assets/images/paddle.png")         ||
+        !BallImage.LoadFromFile("assets/images/ball.png")             ||
+        !BricksImage.LoadFromFile("assets/images/bricks.png"))
     {
-        game = update();
-        draw();
+        return EXIT_FAILURE;
+    }
+
+    std::vector<sf::Sprite> things;
+    sf::Sprite Background(BackgroundImage);
+    sf::Sprite Paddle(PaddleImage);
+    sf::Sprite Ball(BallImage);
+    sf::Sprite Bricks(BricksImage);
+
+    Paddle.Move(325, 550);
+    Ball.Move(390, 290);
+    Bricks.Move(0,100);
+
+    things.push_back(Background);
+    things.push_back(Paddle);
+    things.push_back(Ball);
+    things.push_back(Bricks);
+
+    //bool IsPlaying = true;
+    while (canvas.IsOpened())
+    {
+        // Handle events
+        sf::Event Event;
+        while (canvas.GetEvent(Event))
+        {
+            // Window closed or escape key pressed : exit
+            if ((Event.Type == sf::Event::Closed) || 
+               ((Event.Type == sf::Event::KeyPressed) && (Event.Key.Code == sf::Key::Escape)))
+            {
+                canvas.Close();
+                break;
+            }
+        }
+
+        /*if (isPlaying)
+        {
+            isPlaying = update();
+        }*/
+
+        draw(&canvas, things);
     }
     cleanup();
-    return 0;
+    return EXIT_SUCCESS;
 }
